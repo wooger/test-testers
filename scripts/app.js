02d2a7da80992e06baa0895b33e3d8db551233e8
Paul Mason <pauliMac@Pauls-iMac.home>
2016-07-24 07:52:07 +0100

    Added code to retrieve products - from external JSON datasource

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,17 @@
 
     var app = angular.module('bikeStore', []);    
 
+    var datasource = 'https://raw.githubusercontent.com/jujhars13/jujhars13/master/bikes.json';
+
+    app.controller('prodList', ['$scope', '$http', function ($scope, $http) {
+
+        $http.get(datasource).then(function (response) {
+
+            $scope.products = response.data.items;
+        
+        });
+    }]);
+
 })();
 
 
